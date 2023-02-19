6a7938cc1451717b5eb05f2b84c77335c0abd01c
Serhat SARI <serhhatsari@gmail.com>
2023-02-20 02:33:57 +0300

    implement caching for order service

--- order-service/src/main/java/com/serhat/orderservice/model/dto/response/OrderDto.java
+++ order-service/src/main/java/com/serhat/orderservice/model/dto/response/OrderDto.java
@@ -4,13 +4,14 @@ import com.serhat.orderservice.constant.OrderStatus;
 import lombok.Builder;
 import lombok.Data;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Builder
 @Data
-public class OrderDto {
+public class OrderDto implements Serializable {
 
     private Long id;
 
--- order-service/src/main/java/com/serhat/orderservice/model/dto/response/OrderItemDto.java
+++ order-service/src/main/java/com/serhat/orderservice/model/dto/response/OrderItemDto.java
@@ -5,9 +5,11 @@ import com.serhat.orderservice.model.entity.OrderEntity;
 import lombok.Builder;
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Builder
 @Data
-public class OrderItemDto {
+public class OrderItemDto implements Serializable {
 
     private Long id;
     private Long productId;
