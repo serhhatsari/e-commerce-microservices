76062d8949dc9e30506b5fbcb5ad9d0b650b6b6f
Serhat SARI <serhhatsari@gmail.com>
2023-02-21 03:25:17 +0300

    Revert "implement discovery server - eureka"
    This reverts commit eb51c6277a607227db2dc32e794aadb505468f36.

--- discovery-server/src/main/java/com/serhat/discoveryserver/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/com/serhat/discoveryserver/DiscoveryServerApplication.java
@@ -2,10 +2,8 @@ package com.serhat.discoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
 public class DiscoveryServerApplication {
 
     public static void main(String[] args) {
