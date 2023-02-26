bd83c5b95ab99172f61087acbd4e1d147dec60fe
Serhat SARI <serhhatsari@gmail.com>
2023-02-26 19:18:20 +0300

    update redis connection for customer service

--- customer-service/src/main/java/com/serhat/customerservice/config/RedisConfig.java
+++ customer-service/src/main/java/com/serhat/customerservice/config/RedisConfig.java
@@ -1,24 +1,33 @@
 package com.serhat.customerservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
-@EnableCaching
 @Configuration
+@EnableCaching
 public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String REDIS_HOST;
+    @Value("${spring.data.redis.port}")
+    private Integer REDIS_PORT;
 
     @Bean
     public JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory();
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(REDIS_HOST, REDIS_PORT);
+        return new JedisConnectionFactory(config);
     }
 
     @Bean
-    public RedisTemplate<?,?> redisTemplate() {
-        RedisTemplate<byte[], byte[]> template = new RedisTemplate<>();
-        template.setConnectionFactory(jedisConnectionFactory());
-        return template;
+    public RedisTemplate<?, ?> redisTemplate(){
+        RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(jedisConnectionFactory());
+        return redisTemplate;
     }
+
 }
