4b190ebf1566f287af95287fd6d9e2a9d8f216c4
Serhat SARI <serhhatsari@gmail.com>
2023-03-04 14:51:41 +0300

    implement consumer for product service

--- product-service/src/main/java/com/serhat/productservice/consumer/OrderConsumer.java
+++ product-service/src/main/java/com/serhat/productservice/consumer/OrderConsumer.java
@@ -1,16 +1,30 @@
 package com.serhat.productservice.consumer;
 
+import com.serhat.productservice.exception.ProductNotFoundException;
 import com.serhat.productservice.model.event.OrderPlacedEvent;
+import com.serhat.productservice.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
+@RequiredArgsConstructor
 public class OrderConsumer {
+
+    private final ProductRepository productRepository;
 
     @KafkaListener(topics = "order-events")
     public void consume(OrderPlacedEvent orderPlacedEvent) {
-        log.info("OrderPlacedEvent consumed: {}", orderPlacedEvent);
+        log.debug("OrderPlacedEvent consumed: {}", orderPlacedEvent);
+        var orderItems = orderPlacedEvent.getOrderItems();
+        orderItems.forEach(orderItem -> {
+            log.debug("OrderItem consumed: {}", orderItem);
+            var product = productRepository.findById(orderItem.getProductId()).orElseThrow(ProductNotFoundException::new);
+            product.setStock(product.getStock() - orderItem.getQuantity());
+            productRepository.save(product);
+        });
+        log.info("OrderPlacedEvent consumed successfully: {}", orderPlacedEvent);
     }
 }
